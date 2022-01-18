db02244bc51dd33ab4dee8810ae974badf76f419
student <shabanov089>
2022-01-18 21:44:26 +0200

    lesson#1

---
+++ hello.py
@@ -0,0 +1,30 @@
+number_1 = int(input('enter your first number'))
+number_2 = int(input('enter your second number'))
+operator = input('input [-,+,/,*]')
+if operator == '-':
+    print(number_1-number_2)
+elif operator == '+':
+    print(number_1+number_2)
+elif operator == '/':
+    print(number_1/number_2)
+elif operator == '*':
+    print(number_1*number_2)
+else:
+    operator = '/0'
+    print('valid number')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
