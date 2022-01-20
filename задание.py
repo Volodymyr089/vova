9a4cc0d5f80d7e13626d7b6cdfafa6975ac578cb
student <shabanov089>
2022-01-20 21:55:44 +0200

    lesson#1

---
+++ задание.py
@@ -0,0 +1,9 @@
+x = int(input('введите число: '))
+y = int(input('введите число 2: '))
+z = int(input('введите число 3: '))
+print(x+(y*z))
+
+name = 'vova'
+name2 = 'dima'
+print(name != name2)
+print(name == name2)
