00297dd18bf862d024ac131aa382b132ff6fdbe0
student <shabanov089@gmail.com>
2022-02-01 21:02:44 +0200

    lesson#1

---
+++ Lesson4.py
@@ -0,0 +1,35 @@
+"""
+while True:
+    x = int(input("number1: "))
+    y = int(input("number2: "))
+    if y == 0:
+        print("error")
+        continue
+    print(x/y)
+"""
+
+
+"""
+while True:
+    x = input("Угадай имя: ")
+    if x == "Vova":
+        print("Угадали")
+    else:
+        print("Не Угадали!")
+    break
+"""
+
+
+
+"""
+c = 0
+while True:
+    x = input("Угадай имя: ")
+    c += 1
+    if x == "Vova":
+        print("Вы угадали!")
+        break
+    elif c == 10:
+        print("Вы проиграли!")
+        break
+"""
