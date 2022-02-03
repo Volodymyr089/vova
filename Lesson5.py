859dc347b2167d78e1a1ee48b516b513de973650
student <shabanov089@gmail.com>
2022-02-03 21:28:23 +0200

    lesson#1

--- Lesson5.py
+++ Lesson5.py
@@ -5,10 +5,14 @@ while True:
     if y == "Все":
         break
     x.append(y)
-
+print("Вечеринка началась!")
 while True:
-        c = input("Введите имя гостя: ")
-        if c in x:
-            print(c,  "есть в списке")
-        else:
-            print("Нет в списке!")
+    c = input("Введите имя гостя: ")
+    if c in x:
+        print(c, "есть в списке")
+        x.remove(c)
+    else:
+        print("Нет в списке!")
+    if len(x) == 0:
+        break
+print("Все гости на месте!")
